135145e4a94d566288dfec0dc0d941ac90ae988c
ebayboy <ebayboy@163.com>
2023-01-13 22:43:55 +1100

    commit test

---
+++ src/cpp20_5_edition/chapter_1/test.cpp
@@ -0,0 +1,63 @@
+/****************************************************************************
+@file:limit_test.cpp 
+@author:ebayboy@163.com 
+@date:2023-01-13 22:23 
+@version: 1.0  
+@description: cpp file 
+@Copyright (c)  all right reserved 
+**************************************************************************/
+
+#include <iostream>
+#include <string>
+#include <numeric>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+#define DEBUG 
+
+int main(int argc, char **argv)
+{
+	//max_min_val:  numeric_limits<T>::max()  min() lowest() 用法
+	cout << "max int:" << numeric_limits<int>::max() << endl;
+	cout << "min int:" << numeric_limits<int>::min() << endl;
+	cout << "lowest int:" << numeric_limits<int>::lowest() << endl;
+
+	cout << "max double:" << numeric_limits<double>::max() << endl; //最大正数
+	cout << "min double:" << numeric_limits<double>::min() << endl; //最小正数
+	cout << "lowest double:" << numeric_limits<double>::lowest() << endl; //最小负数
+
+	//Zero init: {}
+	int myInt {};
+	float myFloat {};
+	char myChar {};
+	cout << "myInt:" << myInt << endl;
+	cout << "myFloat:" << myFloat << endl;
+	cout << "myChar:" << myChar << endl;
+
+	//type convert: static_cast<T>()
+	float f1 { 3.14f };
+	int i1 { static_cast<int>(f1) };
+	cout << "i1:" << i1 << endl;
+
+	//inf(无穷) && nan(非数字)
+	float i3 { numeric_limits<float>::infinity() };
+	float i4 { 3.14f };
+	float i2 { i4 };
+	if (isnan( 3.14f )) {
+		cout << "isnan!\n";
+	} else {
+		cout << "isnan : false!\n";
+	}
+	if (isinf(i2)) {
+		cout << "isinf!\n";
+	} else {
+		cout << "isinf : false\n";
+	}
+	cout << "i2:" << i2 << endl;
+
+	return 0;
+}
+
