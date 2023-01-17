a5d9c39b8e3bde5a2da7cf431ab2631600d400c3
ebayboy <ebayboy@163.com>
2023-01-17 15:51:56 +1100

    fw

---
+++ src/employee/main.cpp
@@ -0,0 +1,29 @@
+/****************************************************************************
+@file:main.cpp 
+@author:ebayboy@163.com 
+@date:2023-01-17 15:40 
+@version: 1.0  
+@description: cpp file 
+@Copyright (c)  all right reserved 
+**************************************************************************/
+
+#include <iostream>
+
+import employee;
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+	Employee e;
+
+	e.firstInitial = 'J';
+	e.lastInitial = 'D';
+	e.employeeNumber = 42;
+	e.salary = 8000;
+
+	cout << "e.firstInitial:" << e.firstInitial << endl;
+
+	return 0;
+}
+
