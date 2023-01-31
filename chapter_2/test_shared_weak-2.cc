258e60e4721c2d9c75af81317b23d3180c852588
fanpengfei1 <fanpengfei1@jd.com>
2023-01-31 19:36:31 +0800

    commit test_shared_weak-2

--- chapter_2/test_shared_weak-2.cc
+++ chapter_2/test_shared_weak-2.cc
@@ -30,6 +30,15 @@ void useSimple(weak_ptr<Simple>& weak_simple)
 	}
 }
 
+//shared_ptr<T>作为函数参数时， 无论是值传递还是引用传递都增加引用计数， 两者效果一样。
+//void useSimpleShared(shared_ptr<Simple>& s)
+void useSimpleShared(shared_ptr<Simple> s)
+{
+	s->m_data++;
+	fmt::print("useSimple alive! m_data:{} use_count:{}\n", s->m_data, s.use_count());
+}
+
+
 /*
 construct...10
 share_simple: 10 use_count:1
@@ -39,6 +48,7 @@ useSimple freed!
 */
 int main(int argc, char **argv)
 {
+#if 0
 	auto share_simple { make_shared<Simple>(10) };
 	//访问类成员用->, 访问shared_ptr成员函数用.
 	fmt::print("share_simple: {} use_count:{}\n", share_simple->m_data, share_simple.use_count());
@@ -48,7 +58,12 @@ int main(int argc, char **argv)
 
 	share_simple.reset();
 	useSimple(weak_simple);
-
+#else
+	shared_ptr<Simple> s2 { make_shared<Simple>(20) };	
+	fmt::print("use_count:{} m_data:{}\n", s2.use_count(), s2->m_data);
+	useSimpleShared(s2);
+	fmt::print("use_count:{} m_data:{}\n", s2.use_count(), s2->m_data);
+#endif
 	return 0;
 }
 
