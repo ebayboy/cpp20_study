fea940238e41d7e1a8d18cc065d804830d7d4673
fanpengfei1 <fanpengfei1@jd.com>
2023-01-31 19:07:58 +0800

    commit test_shared_weak-2

--- chapter_2/test_shared_weak-2.cc
+++ chapter_2/test_shared_weak-2.cc
@@ -20,22 +20,27 @@ public:
 	int m_data;
 };
 
-void useSimple(weak_ptr<Simple> & s)
+void useSimple(weak_ptr<Simple>& weak_simple)
 {
-	auto r { s.lock() };
-	if (r) {
-		fmt::print("alive!\n");
+	auto s = weak_simple.lock(); //lock() 返回share_ptr类型
+	if (s) {
+		fmt::print("useSimple alive! m_data:{} use_count:{}\n", s->m_data, s.use_count());
 	} else {
-		fmt::print("freed!\n");
+		fmt::print("useSimple freed!\n");
 	}
-
 }
 
 int main(int argc, char **argv)
 {
-	auto s1 { make_shared<Simple>(10) };
+	auto share_simple { make_shared<Simple>(10) };
+	//访问类成员用->, 访问shared_ptr成员函数用.
+	fmt::print("share_simple: {} use_count:{}\n", share_simple->m_data, share_simple.use_count());
 
-	useSimple(s1);
+	weak_ptr<Simple> weak_simple { share_simple };
+	useSimple(weak_simple);
+
+	share_simple.reset();
+	useSimple(weak_simple);
 
 	return 0;
 }
