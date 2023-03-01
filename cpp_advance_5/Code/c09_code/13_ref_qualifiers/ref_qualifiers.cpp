3b62a727869a414e25a7e8c73da1ec610f9740ad
fanpengfei1 <fanpengfei@jd.com>
2023-03-01 11:59:28 +0000

    commit ref_qualifiers

--- cpp_advance_5/Code/c09_code/13_ref_qualifiers/ref_qualifiers.cpp
+++ cpp_advance_5/Code/c09_code/13_ref_qualifiers/ref_qualifiers.cpp
@@ -1,26 +1,39 @@
-import <iostream>;
-import <string>;
+#include <iostream>
+#include <string>
 
 using namespace std;
 
 class TextHolder
 {
 public:
-	TextHolder(string text) : m_text{ move(text) } {}
+    TextHolder(string text) : m_text{ move(text) } {}
+
+    //const string& getText() const { return m_text; }
+
+	// 后面&为左值引用限定符
+    const string& getText() const &
+    {
+		cout << "string&" << endl;
+        return m_text;
+    }
 	
-	//const string& getText() const { return m_text; }
-	
-	const string& getText() const & { return m_text; }
-	string&& getText() && { return move(m_text); }
+	// 后面&&为右值引用限定符
+    string&& getText() && 
+	{ 
+		cout << "string&&" << endl;
+		return move(m_text); 
+	}
 
 private:
-	string m_text;
+    string m_text;
 };
 
 int main()
 {
-	TextHolder textHolder{ "Hello world!" };
-	cout << textHolder.getText() << endl;
-	cout << TextHolder{ "Hello world!" }.getText() << endl;
-	cout << move(textHolder).getText() << endl;
+    TextHolder textHolder{ "Hello world!" };
+    cout << textHolder.getText() << endl;
+
+    cout << TextHolder{ "Hello world!" }.getText() << endl;
+
+    cout << move(textHolder).getText() << endl;
 }
--- cpp_advance_5/Code/c09_code/README.txt
+++ cpp_advance_5/Code/c09_code/README.txt
@@ -1,2 +1,6 @@
 Unless otherwise noted, include all .cpp files in a subdirectory in your
 project.
+
+
+* 13_ref对应第9章内容
+
