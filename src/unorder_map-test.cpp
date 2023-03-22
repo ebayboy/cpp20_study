c3f53aee3f5a21f2e13f85296c00f427d7eae77e
fanpengfei1 <fanpengfei@jd.com>
2023-03-22 10:31:43 +0000

    commit unorder_map-test

--- src/unorder_map-test.cpp
+++ src/unorder_map-test.cpp
@@ -9,21 +9,37 @@
 using namespace std;
 
 int main(int argc, char **argv) {
-    std::unordered_map<string, string> um;
+    unordered_map<string, string> um;
 
+    // 1. insert
     um.insert(make_pair("key1", "val1"));
     um.insert(make_pair("key2", "val2"));
     um.insert(make_pair("key3", "val3"));
 
+    // 2. find
     auto ret = um.find("key2");
     if (ret != um.end()) {
 	fmt::println("find:{}:{}", ret->first, ret->second);
+	// 3. erase
+	um.erase(ret);
     }
 
-    for (auto &&it : um) {
-	fmt::println("first:{} second:{}", it.first, it.second);
+    // 4. Traverse loop show
+    fmt::println("===um==");
+    for (auto it = begin(um); it != end(um); it++) {
+	fmt::println("first:{} second:{}", it->first, it->second);
     }
 
+    unordered_map<int, string> ium;
+    ium.insert(std::make_pair(1, "val1"));
+    ium.insert(std::make_pair(2, "val2"));
+    ium.insert(std::make_pair(3, "val3"));
+
+    // int类型key
+    fmt::println("===ium==");
+    for (auto it = begin(ium); it != end(ium); it++) {
+	fmt::println("ium first:{} second:{}", it->first, it->second);
+    }
     return 0;
 }
 
