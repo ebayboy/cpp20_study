57c1b6c10d07cb8cd618bf44d1994ab6c391d255
fanpengfei1 <fanpengfei@jd.com>
2023-03-27 12:15:23 +0000

    commit 3order_class-test

---
+++ src/3order_class-test.cpp
@@ -0,0 +1,36 @@
+
+#include <fmt/core.h>
+
+#include <iostream>
+#include <utility>
+
+using namespace std;
+
+// 三向比较运算符以及在类操作符重载中中的应用
+
+class Demo {
+public:
+    Demo(int v) : m_value(v){};
+    ~Demo(){};
+    int getValue() const { return m_value; };
+    auto operator==(Demo const &r) { return (r.getValue() == getValue()); };
+    auto operator<=>(Demo const &r) { return (r.getValue() <=> getValue()); };
+
+private:
+    /* data */
+    int m_value;
+};
+
+int main(int argc, char **argv) {
+    Demo d1{1};
+    Demo d2{2};
+
+    auto res = (d1 == d2);
+    fmt::println("res:{}", res);
+
+	auto res1 = d1 > d2;
+    fmt::println("res1:{}", res1);
+
+    return 0;
+}
+
