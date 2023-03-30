8081402aa254b5f4167b46ec43bebf35f584b2e3
fanpengfei1 <fanpengfei@jd.com>
2023-03-30 11:39:47 +0000

    commit class_method_override

--- src/class_method_override.cpp
+++ src/class_method_override.cpp
@@ -8,6 +8,9 @@ using namespace std;
 class Base {
 	public:
 		virtual void method1() { fmt::println("Base method1"); }
+		virtual void method2() { fmt::println("Base method2"); }
+
+		void method3() { fmt::println("Base method3"); }
 	
 	private:
 		int m_data;
@@ -15,15 +18,37 @@ class Base {
 
 class Drived : public Base {
 	public:
-		// override 必须与 virtual 配对使用
+
+		// override关键字, 显示重写： override 必须与 virtual 配对使用
 		void method1() override { fmt::println("Drived method1"); }
+
+		//隐式重写, 没有override关键字，不推荐
 		void method2() { fmt::println("Drived method2"); }
+		
+		void method3() { fmt::println("Drived method3"); }
 };
 
 int main(int argc, char **argv)
 {
+	// 1. 方法重写
 	Drived d;
-	d.method1();
+	fmt::println("{}:{}", __func__, __LINE__);
+	d.method1(); //Drived method1
+	d.method2(); //Drived method2
+
+	fmt::println("{}:{}", __func__, __LINE__);
+	Base & b {d};
+	b.method1(); //Drived method1
+	b.method2(); //Drived method2
+
+	fmt::println("{}:{}", __func__, __LINE__);
+	// 将Drived转换为Base对象
+	Base b1 {d};
+	b.method1(); //Drived method1
+	
+	fmt::println("{}:{}", __func__, __LINE__);
+	Base b2;
+	b2.method1(); //Base method1
 
 	return 0;
 }
