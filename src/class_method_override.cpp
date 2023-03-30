8d3fe6b8b24384f815fc688977167c24880d1dcf
fanpengfei1 <fanpengfei@jd.com>
2023-03-30 11:26:26 +0000

    commit class_method_override

--- src/class_method_override.cpp
+++ src/class_method_override.cpp
@@ -5,17 +5,25 @@
 
 using namespace std;
 
-class Demo {
+class Base {
 	public:
-		method1() {}
+		virtual void method1() { fmt::println("Base method1"); }
+	
 	private:
 		int m_data;
 };
 
+class Drived : public Base {
+	public:
+		// override 必须与 virtual 配对使用
+		void method1() override { fmt::println("Drived method1"); }
+		void method2() { fmt::println("Drived method2"); }
+};
+
 int main(int argc, char **argv)
 {
-	string s { fmt::format("my {} is {}", "age", 24) };
-	fmt::print("s: {}\n", s);
+	Drived d;
+	d.method1();
 
 	return 0;
 }
