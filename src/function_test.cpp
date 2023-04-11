036a5c28275255eeec21e3883ac95fd7320bbaee
fanpengfei1 <fanpengfei@jd.com>
2023-04-11 08:40:06 +0000

    commit function_test

--- src/function_test.cpp
+++ src/function_test.cpp
@@ -5,15 +5,63 @@
 
 using namespace std;
 
+//1. 
 void func(void)
 {
 	cout << "func\n";
 }
+using UFUNC = void (*)(void);
+
+//2.
+class Foo {
+public:
+	void operator()(void)
+	{
+		cout << "Foo.operator()\n";
+	};
+};
+
+//3. 一个可被转换为函数指针的类对象
+class Bar {
+	public:
+		//定义fr_t为函数指针
+		using fr_t = void(*)(void);
+		static void func(void) { };
+
+		// 重新操作符()，返回函数指针
+		operator fr_t(void) 
+		{
+			cout << "Bar.fr_t()\n";
+			return func; //返回函数指针
+		};
+};
+
+//4. 类的成员函数
+class A {
+	public:
+		void mem_func(void)
+		{
+			cout << "A.mem_func\n";
+		};
+};
 
 int main(int argc, char **argv)
 {
-	using func_ptr = void (*)(void);
-	func_ptr = func;
+	// 1. func函数指针
+	UFUNC uf = func;
+	uf();
+
+	//2. 实现操作符()的类
+	Foo foo;
+	foo();
+
+	//3. 一个可被转换为函数指针的类对象
+	Bar b;
+	b();
+
+	//4. 类成员函数
+	A a;
+	a.mem_func();
 
 	return 0;
 }
