e3b9512dc30c67155055a221aa75025bab6e5168
fanpengfei1 <fanpengfei@jd.com>
2023-04-23 02:39:13 +0000

    commit class_overload_private

--- src/class_overload_private.cpp
+++ src/class_overload_private.cpp
@@ -1,30 +1,31 @@
 
+#include <fmt/core.h>
+
 #include <iostream>
 #include <utility>
-#include <fmt/core.h>
 
 using namespace std;
 
 class Base {
-	public:
-		Base(): m_gallons(2) {};
-		Base(int i) : m_gallons(i) {};
-		void virtual getMilesLeft() { };
-	private:
-		int m_gallons { 0 };
-		int getMilesPerGallon() {
-			return 20;	
-		};
+public:
+    Base() : m_gallons(2){};
+    Base(int i) : m_gallons(i){};
+    int virtual getMilesLeft(){ return getMilesPerGallon() * m_gallons; };
+
+private:
+    int m_gallons{0};
+    int getMilesPerGallon() { return 20; };
 };
 
 class Drived : public Base {
-	public:
+public:
 };
 
-int main(int argc, char **argv)
-{
-	Base b;
+int main(int argc, char **argv) {
+    Base b{2};
 
-	return 0;
+
+
+    return 0;
 }
 
