f5f85d218da78b85968a43eb58d02f746105ed1b
fanpengfei1 <fanpengfei@jd.com>
2023-04-23 03:40:22 +0000

    commit class_overload1

--- src/class_overload1.cpp
+++ src/class_overload1.cpp
@@ -5,6 +5,8 @@
 
 using namespace std;
 
+// 参数默认值不会被重写, 参数默认值是在编译期决定的
+
 class Base {
 	public:
 		void virtual go(int a = 2) { cout << "Base go:" << a << endl; };
@@ -12,6 +14,7 @@ class Base {
 
 class Drived : public Base {
 	public:
+		//重写基类具有默认参数的方法， 默认参数值应保持相同  a = 2
 		void go(int a = 7) override { cout << "Drived go:" << a << endl; };
 };
 
@@ -24,7 +27,7 @@ int main(int argc, char **argv)
 	d.go();
 
 	Base &b {d}; //代码段指向的是Drived的代码段
-	b.go();
+	b.go(); //默认参数值不会被重写
 
 	return 0;
 }
