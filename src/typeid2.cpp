c377ab42115f4d23302abb8ca5d77d33c907d43d
fanpengfei1 <fanpengfei@jd.com>
2023-04-25 11:58:06 +0000

    commit typeid2

--- src/typeid2.cpp
+++ src/typeid2.cpp
@@ -7,9 +7,10 @@
 
 using namespace std;
 
+// typeid用法
+
 // 抽象类
 class Loggable {
-    virtual ~Loggable() = default;
     virtual string getLogMessage() const = 0;  // 纯虚函数
 };
 
@@ -18,10 +19,19 @@ public:
     string getLogMessage() const override { return "Hello logger!"; };
 };
 
-void logObject(const Loggable &logableObject) {
-    cout << typeid(logableObject).name() << ":";
-    cout << logableObject.getLogMessage() << endl;
+void logObject(const Foo &foo) {
+    cout << "typeid.name():" << typeid(foo).name() << endl;
+    cout << "getLogMessage:" << foo.getLogMessage() << endl;
 }
 
-int main(int argc, char **argv) { return 0; }
+int main(int argc, char **argv) { 
+
+	Foo fo;
+
+	cout << "fo.getLogMessage:" << fo.getLogMessage() << endl;;
+
+	logObject(fo);
+
+	return 0; 
+}
 
