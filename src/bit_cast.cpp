c94698a77cce2a850e94a8b110e528cd90cd737e
fanpengfei1 <fanpengfei@jd.com>
2023-05-04 11:20:17 +0000

    commit bit_cast

--- src/bit_cast.cpp
+++ src/bit_cast.cpp
@@ -10,7 +10,12 @@ int main(int argc, char **argv)
 {
 	float a { 1.23f };
 
-	fmt::println("a:{} bit_cast<float>:{} static_cast<int>:{} bit_cast<int>:{}", a, bit_cast<float>(a), static_cast<unsigned int>(a), bit_cast<int>(a));
+	unsigned int b = bit_cast<unsigned int>(a);
+	unsigned int s = static_cast<unsigned int>(a);
+
+	float new_a = bit_cast<float>(b); //bit_cast按位复制， 可以转回去
+
+	fmt::println("a:{} static_cast<int>:{} bit_cast<int>:{} new_a:{}", a, s, b, new_a);
 
 	return 0;
 }
